338f89539783d10b47d7177659263375ba4b96e0
hslim <hasan.slim@gmail.com>
2018-10-05 12:25:56 +0300

    Learning streams of java 8

---
+++ src/main/java/org/slim/hackerrank/StreamsJ8.java
@@ -0,0 +1,56 @@
+package org.slim.hackerrank;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class StreamsJ8 {
+
+
+    // Complete the migratoryBirds function below.
+    static int migratoryBirds(List<Integer> arr) {
+        HashMap<Integer,Integer> freq = new HashMap();
+
+        arr.forEach(elem -> {
+            if(freq.containsKey(elem)) freq.put(elem, freq.get(elem) + 1);
+            else freq.put(elem, 1);
+        });
+
+        Map.Entry<Integer, Integer> maxFrequencyMapEntry = freq.entrySet().stream().
+                sorted((x,y) -> {
+                    return x.getValue() > y.getValue() ? 1 : -1;
+                }).
+                max((x,y) -> {
+                    return x.getValue() > y.getValue() ? 1 : -1;
+                }).orElseGet(null);
+
+        return maxFrequencyMapEntry.getKey();
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int arrCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int result = migratoryBirds(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
